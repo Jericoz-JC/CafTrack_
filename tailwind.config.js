721bb94dbe246ca4ea1b86f8e38d7cf1b88a3b13
Jericoz-JC <chrisgems5@gmail.com>
2025-03-06 18:08:49 -0800

    tailwind css error fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Custom colors if needed
+      },
+    },
   },
   plugins: [],
 } 
